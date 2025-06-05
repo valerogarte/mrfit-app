652aa5040b4aa007731bed5d17ff448bfa4c9a30
Dan Valero <73557314+valerogarte@users.noreply.github.com>
2025-06-05 10:39:32 +0200

    feat: add plugin registrant stub

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,14 @@
+// Generated file. Do not edit.
+
+// ignore_for_file: directives_ordering, comment_references
+
+import 'package:flutter/widgets.dart';
+
+/// Registers plugins for background isolates.
+class DartPluginRegistrant {
+  @pragma('vm:entry-point')
+  static void ensureInitialized() {
+    WidgetsFlutterBinding.ensureInitialized();
+  }
+}
+
